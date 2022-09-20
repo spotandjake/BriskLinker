b6d25e2df3022c3bf13fb5ec4093cf2e40286d5b
Spotandjake <spotandjake@hotmail.com>
2022-09-20 16:39:41 -0400

    We Linked A Program

--- Build.sh
+++ Build.sh
@@ -1,22 +1,26 @@
 # Build File To Run The Project
-while getopts c: flag
+while getopts d:c: flag
 do
     case "${flag}" in
         c) check=${OPTARG};; 
+        d) detail=${OPTARG};;
     esac
 done
 clear
 bash ./clean.sh
 rm ./LinkerTest/Linker_Linked.br.wasm
 grain index.gr
-if [ "$check" == "true" ]; then
-  wasm2wat ./LinkerTest/Linker_Linked.br.wasm -o ./LinkerTest/Linker_Linked.br.wat --verbose
-else
-  wasm2wat ./LinkerTest/Linker_Linked.br.wasm -o ./LinkerTest/Linker_Linked.br.wat --verbose --no-check
+if [ "$detail" == "true" ]; then
+  if [ "$check" == "true" ]; then
+    wasm2wat ./LinkerTest/Linker_Linked.br.wasm -o ./LinkerTest/Linker_Linked.br.wat --verbose
+  else
+    wasm2wat ./LinkerTest/Linker_Linked.br.wasm -o ./LinkerTest/Linker_Linked.br.wat --verbose --no-check
+  fi
 fi
 # Log The Size
 filesize=$(stat -f%z index.gr.wasm)
 filesize=$(bc <<< "scale=3; $filesize / 1024")
 echo "$filesize"kb >> oldSize.txt
+echo "$filesize"kb
 # Try and run
 wasmtime ./LinkerTest/Linker_Linked.br.wasm
